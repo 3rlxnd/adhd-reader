e9a3eb59df9649dce85966c6d609c5c76a5c8078
3rlxnd <erlnd.gdt@gmail.com>
2025-01-07 16:57:36 +0100

    Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,8 @@ import "./Header.css";
 
 function Header() {
     return <header>
-        <img id="logo" src="/logo.png" alt="Logo"/>
+        {/* <img id="logo" src="/logo.png" alt="Logo"/> */}
+        <h1>ADA</h1>
         <nav id="nav">
                 <a aria-label="Coverter" href="/interface" className="nav_link">Coverter</a>
                 <a href="/docs" className="nav_link">Language</a>
