28461389f91d86b4547c8d69335535a07231d059
3rlxnd <erlnd.gdt@gmail.com>
2025-01-10 14:14:14 +0100

    Updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
         {/* <img id="logo" src="/logo.png" alt="Logo"/> */}
         <div style={{display: 'flex', alignItems: 'center', gap: '20px'}}>
             <i id="logo" className="fa-solid fa-eye"></i>
-            <p style={{margin: 0}}>ADHD Reader</p>
+            <p style={{margin: 0}}><b>ADHD</b> Reader</p>
         </div>
         <nav id="nav">
             <Theme></Theme>
